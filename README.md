fce9538c632434fc1aa9ecd2bb14c86baf4bf038
Chris A. Taylor <chris@gameclosure.com>
2013-06-12 13:38:08 -0700

    Billing PLugin

---
+++ README.md
@@ -0,0 +1,12 @@
+# Game Closure DevKit Plugin: Billing
+
+## Usage
+
+Include it in the `manifest.json` file under the "addons" section for your game:
+
+~~~
+"addons": [
+	"billing"
+],
+~~~
+
---
+++ js/install.js
@@ -0,0 +1,117 @@
+// If on true native mobile platform,
+if (!GLOBAL.NATIVE || device.simulatingMobileNative) {
+	logger.log("{billing} Installing fake billing API");
+} else {
+	logger.log("{billing} Installing JS billing component for native");
+
+	var purchasing = {};
+	var onPurchase = {};
+
+	var gotOwned = false;
+	var onOwned = [];
+	var ownedSet = {};
+	var ownedArray = [];
+
+	NATIVE.events.registerHandler('billingPurchase', function(evt) {
+		// NOTE: Function is organized carefully for callback reentrancy
+
+		// If not failed,
+		if (!evt.failure) {
+			// Mark it owned
+			owned[evt.sku] = 1;
+		}
+
+		// If purchase callbacks are installed,
+		var calls = onPurchase[evt.sku];
+		if (calls && calls.length > 0) {
+			// For each callback,
+			for (var ii = 0; ii < calls.length; ++ii) {
+				// Run it
+				calls[ii](evt.failure);
+			}
+
+			// Clear callbacks
+			calls.length = 0;
+		}
+
+		// Disable purchasing flag
+		purchasing[evt.sku] = 0;
+	});
+
+	NATIVE.events.registerHandler('billingOwned', function(evt) {
+		// Add owned items
+		var skus = evt.skus;
+		if (skus && skus.length > 0) {
+			for (var ii = 0, len = skus.length; ii < len; ++ii) {
+				var sku = skus[ii];
+
+				ownedSet[sku] = 1;
+				ownedArray.push(sku);
+			}
+		}
+
+		gotOwned = true;
+
+		// Call owned callbacks
+		for (var ii = 0; ii < onOwned.length; ++ii) {
+			onOwned[ii](ownedArray);
+		}
+		onOwned.length = 0;
+	});
+
+	GLOBAL.billing = {
+		isPurchased: function(sku, next) {
+			if (typeof(next) == "function") {
+				// If already got owned list,
+				if (gotOwned) {
+					// Complete immediately
+					next(ownedSet[sku] == 1);
+				} else {
+					// Add to callback list
+					onOwned.push(function() {
+						next(ownedSet[sku] == 1);
+					});
+				}
+			}
+		},
+		purchase: function(sku, next) {
+			// If already waiting for a purchase callback,
+			if (purchasing[sku] == 1) {
+				if (typeof(next) == "function") {
+					if (onPurchase[sku]) {
+						onPurchase[sku].push(next);
+					} else {
+						onPurchase[sku] = [next];
+					}
+				}
+			} else {
+				// We are now purchasing it
+				purchasing[sku] = 1;
+
+				if (typeof(next) == "function") {
+					onPurchase[sku] = [next];
+				}
+
+				// Kick it off
+				NATIVE.plugins.sendEvent("BillingPlugin", "purchase", JSON.stringify({
+					"sku": sku
+				}));
+			}
+		},
+		getPurchases: function(next) {
+			// If already got owned list,
+			if (gotOwned) {
+				// Complete immediately
+				next(ownedArray);
+			} else {
+				if (typeof(next) == "function") {
+					// Add to callback list
+					onOwned.push(next);
+				}
+			}
+		}
+	}
+
+	NATIVE.plugins.sendEvent("BillingPlugin", "getPurchases", "{}"));
+}
+
