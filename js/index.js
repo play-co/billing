dd69ae701181719ae33a98195cca146e54336b84
Joe Wilm <joe@gameclosure.com>
2014-09-26 21:40:34 -0700

    Use faux billing for debug builds

--- js/index.js
+++ js/index.js
@@ -231,7 +231,7 @@ function onMarketStateChange() {
 }
 
 // If just simulating native device,
-if (!GLOBAL.NATIVE || device.isSimulator) {
+if (!GLOBAL.NATIVE || device.isSimulator || DEBUG) {
 	logger.log("Installing fake billing API");
 } else {
 	logger.log("Installing JS billing component for native");
